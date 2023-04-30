fb6b733e34e18bb3ab6722d3fbe8a3ff233542ee
esquith <esquithallen@gmail.com>
2023-04-30 13:56:25 -0400

    Add a resolved boolean to Incidents

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_230_430_174_158) do
+ActiveRecord::Schema[7.0].define(version: 20_230_430_175_442) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
@@ -24,5 +24,6 @@ ActiveRecord::Schema[7.0].define(version: 20_230_430_174_158) do
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
     t.string 'status'
+    t.boolean 'resolved'
   end
 end
