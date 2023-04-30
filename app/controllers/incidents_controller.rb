7ab6c310e6bec4e35a9ed6bb69880c541214dcbf
esquith <esquithallen@gmail.com>
2023-04-29 20:58:21 -0400

    Add create method code

--- app/controllers/incidents_controller.rb
+++ app/controllers/incidents_controller.rb
@@ -4,4 +4,20 @@ class IncidentsController < ApplicationController
   def index
     @incidents = Incident.all
   end
+
+  def create
+    @incident = Incident.new(incident_params)
+
+    if @incident.save
+      redirect_to @incident
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def incident_params
+    params.require(:incident).permit(:title, :description, :location)
+  end
 end
