c449cf18737dcfb6478dd793296d4f1d2463f595
esquith <esquithallen@gmail.com>
2023-04-29 20:30:22 -0400

    Annotate models

--- app/models/incident.rb
+++ app/models/incident.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: incidents
+#
+#  id          :bigint           not null, primary key
+#  title       :string
+#  description :string
+#  severity    :string
+#  creator     :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 class Incident < ApplicationRecord
 end
--- spec/models/incident_spec.rb
+++ spec/models/incident_spec.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: incidents
+#
+#  id          :bigint           not null, primary key
+#  title       :string
+#  description :string
+#  severity    :string
+#  creator     :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 require 'rails_helper'
 
 RSpec.describe Incident, type: :model do
