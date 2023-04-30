eeb391f31bedeb4e39cbe6c693400907592ba822
esquith <esquithallen@gmail.com>
2023-04-29 20:58:53 -0400

    Make tests pass

--- spec/requests/incidents_spec.rb
+++ spec/requests/incidents_spec.rb
@@ -1,9 +1,20 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+require 'rails-controller-testing'
 
 RSpec.describe 'Incidents', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe 'GET #index' do
+    it 'assigns @incidents' do
+      incident1 = FactoryBot.create(:incident)
+      incident2 = FactoryBot.create(:incident)
+      get '/incidents'
+      expect(assigns(:incidents)).to match_array([incident1, incident2])
+    end
+
+    it 'renders the index template' do
+      get '/incidents'
+      expect(response).to render_template('index')
+    end
   end
 end
