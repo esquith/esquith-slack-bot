d86a1bf97e25c4b5bc726aefcd69c138111b044d
esquith <esquithallen@gmail.com>
2023-04-29 20:35:18 -0400

    Create 3 fake Incidents when seed file is run

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,11 @@
-# frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+# Create 3 fake incidents
+3.times do
+  Incident.create!(
+    title: Faker::Lorem.sentence(word_count: 3),
+    description: Faker::Lorem.paragraph(sentence_count: 2),
+    severity: ['sev0', 'sev1', 'sev2'].sample,
+    creator: Faker::Name.name
+  )
+end
