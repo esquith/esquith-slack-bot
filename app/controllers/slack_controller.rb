d908943003cbd4aae448f768457f58fbad4a359a
esquith <esquithallen@gmail.com>
2023-04-30 22:05:21 -0400

    Add command route and controller method

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class SlackController < ApplicationController
-  skip_before_action :verify_authenticity_token, only: [:declare, :resolve]
+  skip_before_action :verify_authenticity_token, only: %i[declare resolve command]
 
   def declare
     if params[:challenge]
@@ -24,6 +24,28 @@ class SlackController < ApplicationController
         response_type: 'in_channel',
         text: "Incident resolved: #{incident.title}"
       }
+    end
+  end
+
+  def command
+    text = params[:text]
+
+    if text.start_with?('declare ')
+      title = text[8..]
+      incident = Incident.create(title:)
+
+      render json: { response_type: 'in_channel', text: "New incident declared: #{incident.title}" }
+    elsif text == 'resolve'
+      incident = Incident.last
+      incident.update(resolved: true)
+      return unless incident.present?
+
+      render json: {
+        response_type: 'in_channel',
+        text: "Incident resolved: #{incident.title}"
+      }
+    else
+      render json: { response_type: 'ephemeral', text: 'Invalid command. Usage: `/rootly declare <title>` or `/rootly resolve`' }
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@
 Rails.application.routes.draw do
   root to: 'incidents#index'
 
+  post '/slack/command' => 'slack#command'
   post '/slack/resolve' => 'slack#resolve'
   post '/slack/declare' => 'slack#declare'
 
