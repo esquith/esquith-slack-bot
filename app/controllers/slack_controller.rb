8f21637e6ccc40c5eb5e9b5d7b63e3ebb9484406
esquith <esquithallen@gmail.com>
2023-04-30 21:05:16 -0400

    Add declare and resolve methods with tests

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -1,4 +1,20 @@
 # frozen_string_literal: true
 
 class SlackController < ApplicationController
+  def declare
+    incident = Incident.create(title: params[:text])
+
+    render json: { response_type: 'in_channel', text: "New incident declared: #{incident.title}" }
+  end
+
+  def resolve
+    incident = Incident.last
+    incident.update(resolved: true)
+    return unless incident.present?
+
+    render json: {
+      response_type: 'in_channel',
+      text: "Incident resolved: #{incident.title}"
+    }
+  end
 end
--- spec/requests/slack_spec.rb
+++ spec/requests/slack_spec.rb
@@ -3,6 +3,30 @@
 require 'rails_helper'
 
 RSpec.describe 'Slacks', type: :request do
-  describe 'GET /index' do
+  describe 'POST #declare' do
+    it 'creates a new incident and then sends a success message in response' do
+      post '/slack/declare', params: { text: 'Test incident' }
+
+      expect(response).to have_http_status(:success)
+      expect(response.body).to eq({
+        response_type: 'in_channel',
+        text: 'New incident declared: Test incident'
+      }.to_json)
+      expect(Incident.last.title).to eq('Test incident')
+    end
+  end
+
+  describe 'POST #resolve' do
+    it 'resolves the incident and sends a success message in response' do
+      incident = Incident.create(title: 'Test incident')
+      post '/slack/resolve'
+
+      expect(response).to have_http_status(:success)
+      expect(response.body).to eq({
+        response_type: 'in_channel',
+        text: "Incident resolved: #{incident.title}"
+      }.to_json)
+      expect(incident.reload.resolved?).to eq(true)
+    end
   end
 end
